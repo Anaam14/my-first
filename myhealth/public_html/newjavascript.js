ab1f76d51d34a0abaaa29f757a2430b3d733a8b2
Anaam14 <48242401+Anaam14@users.noreply.github.com>
2019-03-05 23:02:08 +0300

    my-first

---
+++ myhealth/public_html/newjavascript.js
@@ -0,0 +1,84 @@
+function myFunction1() {
+    var x = document.getElementById("dcard1");
+    var submit = document.getElementById("Read1");
+    var ask = document.getElementById("ask1");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("dcard2");
+    var submit = document.getElementById("Read2");
+    var ask = document.getElementById("ask2");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("dcard3");
+    var submit = document.getElementById("Read3");
+    var ask = document.getElementById("ask3");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("dcard4");
+    var submit = document.getElementById("Read4");
+    var ask = document.getElementById("ask4");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("dcard5");
+    var submit = document.getElementById("Read5");
+    var ask = document.getElementById("ask5");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
+function myFunction6() {
+    var x = document.getElementById("dcard6");
+    var submit = document.getElementById("Read6");
+    var ask = document.getElementById("ask6");
+    if (x.style.height === "206px" || submit.value === 'Read more') {
+        x.style.height="336px";x.style.border="1px solid black";
+        submit.value = 'Read less';submit.style.top="294px";
+        ask.style.top="294px";
+    } else {
+        x.style.height = "206px";
+        submit.value = 'Read more';submit.style.top="160px";
+        ask.style.top="160px";
+    }
+}
